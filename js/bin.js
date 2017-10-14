99470c9cafbccd999185b280beccdc72167cfc75
jwang206 <jwang206@hawk.iit.edu>
2017-10-13 20:57:17 -0500

    clean 4

---
+++ js/bin.js
@@ -0,0 +1,46 @@
+
+// //add button handler
+// //To add selected books in an array and tempt to save to google doc via API
+// $('#add').on('click', function(e){
+// var selected = [];
+
+// for(var i = 0; i < $('.liChk').length; i++){
+//     if ($('#checkbox'+i).is(':checked')) {
+//       selected.push($('#checkbox'+i).parent()[0].innerText);
+//     }
+// }
+
+// //display selected books in console
+// for( var a = 0; a < selected.length; a++){
+//   console.log(selected[a]);
+// }
+// });//end of #add
+
+
+// //clear button handler 
+// $('#clear').on('click', function(){
+
+//   console.log("clear btn clicked");
+//   for(var i = 0; i < $('.liChk').length; i++)
+//       $('#checkbox'+i).attr('checked', false);
+
+//   // //display selected books in console
+//   // for( var a = 0; a < selected.length; a++){
+//   //   console.log(selected[a]);
+//   // }
+
+// });
+
+
+
+          // //checking checkbox status
+          // $('li #checkbox'+x).on('change', function (){
+
+          //   if ($(this).is(':checked')) {
+          //     $("#actions").css({'width':($("#form").width()+'px')});
+          //     $("#actions").css('margin-top','2em');
+             
+          //     $('#actions').show();
+          //   }
+
+          // });
--- js/site.js
+++ js/site.js
@@ -15,11 +15,13 @@ $(document).ready(function(){ //variables used in this script is protected from 
         parseData(data);
       } else {
         console.log('single object: ');
-    // console.log(data);
+      //console.log(data);
         }
       }
     });//end of ajax
+
     e.preventDefault();
+
   });//end of form
 
 
@@ -72,49 +74,3 @@ $(document).ready(function(){ //variables used in this script is protected from 
 
 });//end of site.js
 
-
-// //add button handler
-// //To add selected books in an array and tempt to save to google doc via API
-// $('#add').on('click', function(e){
-// var selected = [];
-
-// for(var i = 0; i < $('.liChk').length; i++){
-//     if ($('#checkbox'+i).is(':checked')) {
-//       selected.push($('#checkbox'+i).parent()[0].innerText);
-//     }
-// }
-
-// //display selected books in console
-// for( var a = 0; a < selected.length; a++){
-//   console.log(selected[a]);
-// }
-// });//end of #add
-
-
-// //clear button handler 
-// $('#clear').on('click', function(){
-
-//   console.log("clear btn clicked");
-//   for(var i = 0; i < $('.liChk').length; i++)
-//       $('#checkbox'+i).attr('checked', false);
-
-//   // //display selected books in console
-//   // for( var a = 0; a < selected.length; a++){
-//   //   console.log(selected[a]);
-//   // }
-
-// });
-
-
-
-          // //checking checkbox status
-          // $('li #checkbox'+x).on('change', function (){
-
-          //   if ($(this).is(':checked')) {
-          //     $("#actions").css({'width':($("#form").width()+'px')});
-          //     $("#actions").css('margin-top','2em');
-             
-          //     $('#actions').show();
-          //   }
-
-          // });
