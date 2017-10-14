49b8d4733a80e1cd5e50f43e932db33a295bdc8d
jwang206 <jwang206@hawk.iit.edu>
2017-10-14 16:16:14 -0500

    fix finalv1

--- js/site.js
+++ js/site.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){ //variables used in this script is protected from gloable
 
-$( "#selectable" ).selectable();
+$( "#results" ).selectable();
 
   $('#form').on('submit', function(e) {
     var input = $('#inputText').val();
@@ -22,19 +22,16 @@ $( "#selectable" ).selectable();
     e.preventDefault();
   });//end of form
 
-
   function parseData(arr) {   
       //clears out the old resultss, and change the font color
-      $('#selectable').empty();
-      $('#selectable').css('height', '40em');
-      $('#selectable').css('overflow', 'auto');
-      $('#selectable').css('color', 'white');      
+      $('#results').empty();
+  
 
     //display data
       for( var x=0; x < arr.items.length; x++){
 
         try {
-        $('#selectable').append("<li id=li" + x +">" + "<img id=img" +x
+        $('#results').append("<li id=li" + x +">" + "<img id=img" +x
           + " src=" + arr.items[x].volumeInfo.imageLinks.thumbnail + "/>"
           + "<p id=title" + x +">" + (x+1) + ". "+ arr.items[x].volumeInfo.title + "</p>" + "<p id=author" + x +">" 
           + arr.items[x].volumeInfo.authors + "</p></li>");
@@ -63,7 +60,8 @@ $( "#selectable" ).selectable();
 
           //List onclick handler
             $('#li'+x).on("click", function(){
-                confirm("Add this book?");
+                
+                // confirm("Add this book?");
             });
 
       }//end of display data
@@ -72,49 +70,3 @@ $( "#selectable" ).selectable();
 
 });//end of site.js
 
-
-// //add button handler
-// //To add selected books in an array and tempt to save to google doc via API
-// $('#add').on('click', function(e){
-// var selected = [];
-
-// for(var i = 0; i < $('.liChk').length; i++){
-//     if ($('#checkbox'+i).is(':checked')) {
-//       selected.push($('#checkbox'+i).parent()[0].innerText);
-//     }
-// }
-
-// //display selected books in console
-// for( var a = 0; a < selected.length; a++){
-//   console.log(selected[a]);
-// }
-// });//end of #add
-
-
-// //clear button handler 
-// $('#clear').on('click', function(){
-
-//   console.log("clear btn clicked");
-//   for(var i = 0; i < $('.liChk').length; i++)
-//       $('#checkbox'+i).attr('checked', false);
-
-//   // //display selected books in console
-//   // for( var a = 0; a < selected.length; a++){
-//   //   console.log(selected[a]);
-//   // }
-
-// });
-
-
-
-          // //checking checkbox status
-          // $('li #checkbox'+x).on('change', function (){
-
-          //   if ($(this).is(':checked')) {
-          //     $("#actions").css({'width':($("#form").width()+'px')});
-          //     $("#actions").css('margin-top','2em');
-             
-          //     $('#actions').show();
-          //   }
-
-          // });
