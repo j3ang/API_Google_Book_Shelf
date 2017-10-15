de184af144b7ba4279ab492c3b758ac4c4692ba3
jwang206 <jwang206@hawk.iit.edu>
2017-10-15 16:25:40 -0500

    dispaly issue fix2

--- js/site.js
+++ js/site.js
@@ -39,9 +39,11 @@ $('#form').on('submit', function(e) {
         "<p id=title" + x +">" + (x+1) + ". "+ arr.items[x].volumeInfo.title + "</p>" + 
         "<p class=author id=author" + x +">" + arr.items[x].volumeInfo.authors + "</p></li>");
 
-      } catch (TypeError) {
+      } catch (e) {
+        if (e instanceof TypeError){          
         // statements to handle TypeError exceptions
         console.log("image" + x + " TypeError?");            
+        }
       } //end of try/catch blcok
       
       //add link to the image
